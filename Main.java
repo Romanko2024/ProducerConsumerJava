4eb4c1f106a1f48311e6fed6391c032e6d4ec02b
Romanko2024 <Roman_ko2024@outlook.com>
2026-04-08 15:35:54 +0300

    ---

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        int storageSize = 3;
+        int itemNumbers = 10;
+        main.starter(storageSize, itemNumbers);
+    }
+
+    private void starter(int storageSize, int itemNumbers) {
+        Manager manager = new Manager(storageSize);
+        new Consumer(itemNumbers, manager);
+
+        new Producer(itemNumbers, manager);
+    }
+}
